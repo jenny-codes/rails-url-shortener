3d903fcb6be7e9f84a8911e18adc2695597c3ca4
Jenny Shih <jinghua.shih@gmai.com>
2019-01-31 21:35:32 +0800

    redesign routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'urls#index'
   
-  get '/:code', to: 'urls#show', as: 'get_url', constraints: { code: /[A-Z0-9]{4}/ }
-  resources :urls, except: [:index, :edit, :show]
+  get '/:code', to: 'urls#get', as: 'get_url', constraints: { code: /[A-Z0-9]{4}/ }
+  resources :urls, except: [:index, :edit, :update]
 end
